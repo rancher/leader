0125b0bb88d3650135af093fb0af5041279cc306
Bill Maxwell <bill@rancher.com>
2015-12-12 13:05:54 -0700

    Add forward always mode
    This also includes methods to handle proxying in both directions.

--- election/tcp.go
+++ election/tcp.go
@@ -30,6 +30,11 @@ func (t *TcpProxy) Close() error {
 	return nil
 }
 
+func (t *TcpProxy) Reset() error {
+	t.done <- false
+	return nil
+}
+
 func (t *TcpProxy) Forward() error {
 	strAddr := fmt.Sprintf("0.0.0.0:%d", t.port)
 
@@ -45,27 +50,29 @@ func (t *TcpProxy) Forward() error {
 	}
 	defer l.Close()
 
+	logrus.Infof("Forwarding setup to: %s", t.to())
+
 	wg := sync.WaitGroup{}
 
 	for {
-		switch {
+		select {
 		case <-t.done:
 			return nil
 		default:
+			break
 		}
 
 		l.SetDeadline(time.Now().Add(1 * time.Second))
-		conn, err := l.Accept()
+		conn, err := l.AcceptTCP()
 		if acceptErr, ok := err.(*net.OpError); ok && acceptErr.Timeout() {
 			continue
 		}
-
 		if err != nil {
 			return err
 		}
 
 		wg.Add(1)
-		go func(conn net.Conn) {
+		go func(conn *net.TCPConn) {
 			defer wg.Done()
 			if err := t.forward(conn); err != nil {
 				logrus.Errorf("Failed handling TCP forwarding: %v", err)
@@ -77,19 +84,55 @@ func (t *TcpProxy) Forward() error {
 	return nil
 }
 
-func (t *TcpProxy) forward(conn net.Conn) error {
-	defer conn.Close()
+func (t *TcpProxy) forward(cConn *net.TCPConn) error {
+	//cConn is the incoming client connection.
+	defer cConn.Close()
 	ip := t.to()
 	if ip == "" {
 		return errors.New("Target unknown")
 	}
 
-	clientConn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", ip, t.port))
+	raddr, err := net.ResolveTCPAddr("tcp", ip)
 	if err != nil {
 		return err
 	}
-	defer clientConn.Close()
+	rConn, err := net.DialTCP("tcp", nil, raddr)
+	if err != nil {
+		return err
+	}
+	defer rConn.Close()
 
-	_, err = io.Copy(TimeoutConn{clientConn, 2}, TimeoutConn{conn, 2})
+	cClose := make(chan bool)
+	rClose := make(chan bool)
+
+	go connectionBroker(rConn, cConn, cClose)
+	go connectionBroker(cConn, rConn, rClose)
+
+	done := make(chan bool)
+	select {
+	case <-cClose:
+		rConn.SetLinger(0)
+		rConn.CloseRead()
+		done = rClose
+	case <-rClose:
+		cConn.CloseRead()
+		done = cClose
+	}
+
+	<-done
+
 	return err
+}
+
+func connectionBroker(dst, src net.Conn, srcClosed chan bool) {
+	_, err := io.Copy(dst, src)
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	if err := src.Close(); err != nil {
+		logrus.Errorf("Connection close error: %s", err)
+	}
+
+	srcClosed <- true
 }
